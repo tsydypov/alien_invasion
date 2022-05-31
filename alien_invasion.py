01e3de9631297802989b92f5e3441c1a00db8e3a
Цыдыпов Владимир <vov_sed@mail.rugit config --global user.name Vladimir>
2022-06-01 02:52:07 +0300

    code refactoring

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 
 def run_game():
@@ -15,13 +16,8 @@ def run_game():
 
     # Start the main loop for the game.
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-        # Last screen image
-        pygame.display.flip()
+        gf.check_events()
+        gf.update_screens(ai_settings, screen, ship)
 
 
 run_game()
