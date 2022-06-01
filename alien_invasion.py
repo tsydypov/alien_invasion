821d76a9af7b29d8992f5307e58fe43c6d667616
Цыдыпов Владимир <vov_sed@mail.rugit config --global user.name Vladimir>
2022-06-01 03:25:42 +0300

    ship can move now

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,4 +1,3 @@
-import sys
 import pygame
 
 from settings import Settings
@@ -11,12 +10,13 @@ def run_game():
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
     pygame.display.set_caption("Alien Invasion")
 
     # Start the main loop for the game.
     while True:
-        gf.check_events()
+        gf.check_events(ship)
+        ship.update()
         gf.update_screens(ai_settings, screen, ship)
 
 
